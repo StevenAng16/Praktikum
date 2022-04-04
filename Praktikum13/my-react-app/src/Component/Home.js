363504d693b1e0607680c535d316e3f1f79cd574
Steven <100898335+StevenAng16@users.noreply.github.com>
2022-04-04 09:56:46 +0700

    Update Home.js

--- Praktikum13/my-react-app/src/Component/Home.js
+++ Praktikum13/my-react-app/src/Component/Home.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import {v4 as uuidv4} from "uuid"
+import {v4 as uuidv4} from "uuid";
 
-import ListJudul from './ListJudul'
-//import JudulInput from './JudulInput'
-import Header from './Header'
+import './Home.module.css';
+import ListJudul from './ListJudul';
+import JudulInput from './JudulInput';
+import Header from './Header';
 
 const initialValue = [
      {
@@ -18,7 +19,7 @@ const initialValue = [
      },
  ]
 
-function Home (){
+function Home () {
      const [data, setData] = useState(initialValue)
 
     const hapusJudul = id => {
@@ -26,31 +27,32 @@ function Home (){
             return item.id !== id;
         }))
        
-    }
+    };
 
-    // const tambahJudul = newUser => {
-    //      const newData = {
-    //          id : uuidv4(),
-    //           ...newUser
-    //      };
-    //      setData((oldData) => [...oldData, newData]);
-    // };
+    const tambahJudul = newUser => {
+         const newData = {
+             id : uuidv4(),
+              ...newUser
+         };
+         setData((oldData) => [...oldData, newData]);
+    };
 
         return(
-        <div class='home'>
-            <body>
+        <div class='Home'>
             <div>
             <Header />
-            {/* <JudulInput 
+            {/* <!-- CSS only --> */}
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+            {/* <!-- JavaScript Bundle with Popper --> */}
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"/>
+            <JudulInput 
                 tambahJudul={tambahJudul}
-            />  */}
+            />
             <ListJudul 
                 data = {data} 
                 hapusJudul ={hapusJudul}  
             />
             </div>
-            </body>
-            <footer></footer>
         </div>    
         )
     }
